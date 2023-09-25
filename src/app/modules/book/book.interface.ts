4177a3a4c61ef6f0ffd287126ca35bba0d85d437
mitaly-dev <mitalyy.dev@gmail.com>
2023-09-26 01:59:34 +0600

    category added in book interface

--- src/app/modules/book/book.interface.ts
+++ src/app/modules/book/book.interface.ts
@@ -1,11 +1,4 @@
 export type IfilterData = {
   search?: string;
-  studentId?: string;
-  firstName?: string;
-  middleName?: string;
-  email?: string;
-  contactNo?: string;
-  gender?: string;
-  bloodGroup?: string;
-  academicSemeserId?: string;
+  category?: string;
 };
