9cc1b90cb88a1edd98af78d9fc12c950bd858e08
mitaly-dev <mitalyy.dev@gmail.com>
2023-09-26 01:52:12 +0600

    profile service created

---
+++ src/app/modules/profile/profile.service.ts
@@ -0,0 +1,24 @@
+import { prisma } from '../../../shared/prisma';
+import { IProfile } from './profile.interface';
+
+const getProfile = async (userId: string): Promise<IProfile | null> => {
+  const result = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      name: true,
+      email: true,
+      password: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+    },
+  });
+  return result;
+};
+
+export const ProfileService = {
+  getProfile,
+};
